317c0cb203af00ff59c95fade2aad48500cad04b
Simon Hartcher <simon@simonhartcher.com>
2024-11-25 16:36:06 +1100

    chore: export action planner types

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,15 @@
 const std = @import("std");
 const testing = std.testing;
 const astar = @import("astar.zig");
+const ap = @import("action_planner.zig");
 
 pub const Astar = astar.Astar;
 pub const AstarOptions = astar.AstarOptions;
 pub const Result = astar.Result;
+pub const ActionPlanner = ap.ActionPlanner;
+pub const ActionError = ap.ActionError;
+pub const WorldState = ap.WorldState;
+pub const PossibleTransitions = ap.PossibleTransitions;
 
 test {
     testing.refAllDecls(@This());
